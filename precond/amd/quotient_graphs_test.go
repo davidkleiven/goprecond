3b400719b0af0f6d2934ecc73a9de531b0eabaa1
David Kleiven <davidkleiven446@gmail.com>
2025-01-16 22:41:36 +0100

    test: add test that exact method recovers chain

--- precond/amd/quotient_graphs_test.go
+++ precond/amd/quotient_graphs_test.go
@@ -1,6 +1,10 @@
 package amd
 
-import "testing"
+import (
+	"math/rand"
+	"slices"
+	"testing"
+)
 
 func TestQuotientGraph(t *testing.T) {
 	// Build the graph
@@ -55,5 +59,49 @@ func TestQuotientGraph(t *testing.T) {
 		if degree != test.want {
 			t.Errorf("Test %s: wanted %d got %d\n", test.desc, test.want, degree)
 		}
+	}
+}
+
+func TestOrderChain(t *testing.T) {
+	n := 50
+	rnd := rand.New(rand.NewSource(0))
+	nodes := make([]int, n)
+	for i := range nodes {
+		nodes[i] = i
+	}
+
+	rnd.Shuffle(n, func(i, j int) {
+		// Ensure that the largest node is an edge
+		// In that case the minimum degree method
+		// should be able to recover the order of a chain
+		if i != n-1 && j != n-1 {
+			nodes[i], nodes[j] = nodes[j], nodes[i]
+		}
+	})
+
+	adjList := make([][]int, n)
+	for i := range n {
+		current := nodes[i]
+		neighbours := make([]int, 0, 2)
+		if i > 0 {
+			neighbours = append(neighbours, nodes[i-1])
+		}
+		if i < n-1 {
+			neighbours = append(neighbours, nodes[i+1])
+		}
+		adjList[current] = neighbours
+	}
+
+	quotientGraphCalc := QuotientGraphExactDegreeCalculator{}
+	pivot := ApproximateMinimumDegree(n, adjList, &quotientGraphCalc)
+
+	pivotedNodes := make([]int, n)
+	for i, v := range pivot {
+		pivotedNodes[v] = nodes[i]
+	}
+
+	slices.Sort(nodes)
+	if slices.Compare(pivotedNodes, nodes) != 0 {
+		t.Errorf("AMD should be able to recover the order of the 1D chain. Got\n%v\n", pivotedNodes)
 	}
 }
