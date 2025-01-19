f8fe71eebe17f7767ecee65f5b928236b47f4028
David Kleiven <davidkleiven446@gmail.com>
2025-01-19 10:18:24 +0100

    feat(property): add rapid generator for symmetric sparse matrix

--- precond/property/matrices.go
+++ precond/property/matrices.go
@@ -32,6 +32,30 @@ func DenseSquareMatrix(t *rapid.T, minSize int, maxSize int) *mat.Dense {
 	return mat.NewDense(n, n, data)
 }
 
+func SparseSymmetricMatrix(t *rapid.T, minSize int, maxSize int) *mat.SymDense {
+	size := rapid.IntRange(minSize, maxSize).Draw(t, "size")
+	matrix := mat.NewSymDense(size, nil)
+	length := size * size / 3
+	nonZero := rapid.SliceOfNDistinct(rapid.IntRange(0, size*size-1), length, length, func(item int) int { return item }).Draw(t, "non-zero")
+	values := rapid.SliceOfN(rapid.Float64Range(-100.0, 100.0), length, length).Draw(t, "non-zero-values")
+
+	for i := range nonZero {
+		row := i % size
+		col := i / size
+		matrix.SetSym(row, col, values[i])
+	}
+
+	// Fill diagonal in a way that guarantees that the matrix is positive definite
+	for i := 0; i < size; i++ {
+		sum := 0.0
+		for j := 0; j < size; j++ {
+			sum += math.Abs(matrix.At(i, j))
+		}
+		matrix.SetSym(i, i, sum+0.1)
+	}
+	return matrix
+}
+
 type SparseSymmetrixMatrixGenerator struct {
 	Matrix *mat.SymDense
 }
